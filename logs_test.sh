bbe2d485c0b4d84f6cf140e9320b61f12dec4275
Rafael <rafael.trevisan@inf.ufrgs.br>
2024-09-02 21:38:14 +0000

    first test

--- logs_test.sh
+++ logs_test.sh
@@ -1,16 +1,15 @@
 
 mkdir logs_$1
 cd logs_$1
-mkdir logs_$1/mc 
-mkdir logs_$1/v2x
+mkdir mc 
+mkdir well
 
-for type in mc v2x
+for type in mc well
 do
 
-    for num in 0 1 2 3 4 5 6 7 8 9 10
+    for num in 0 1 2 3 4
     do 
     kubectl logs -n dtwins twin-${type}-stateful-${num} > ${type}/twin-${type}-stateful-${num}.log
     done
-    python3 ../main.py
     
 done
