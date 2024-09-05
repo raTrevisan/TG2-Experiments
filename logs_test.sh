f0bdb1b08c99ade228f5b9e14e1ac719aad89354
Rafael <rafael.trevisan@inf.ufrgs.br>
2024-09-05 19:25:42 +0000

    first experiment logs

--- logs_test.sh
+++ logs_test.sh
@@ -7,7 +7,7 @@ mkdir well
 for type in mc well
 do
 
-    for num in 0 1 2 3 4
+    for num in 0 1 2 3 4 5 6 7 8 9 10
     do 
     kubectl logs -n dtwins twin-${type}-stateful-${num} > ${type}/twin-${type}-stateful-${num}.log
     done
