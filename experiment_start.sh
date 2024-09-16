96521c7dd1d8f7c73772d9cb8a505ef3d0c4066e
Rafael <rafael.trevisan@inf.ufrgs.br>
2024-09-16 21:06:48 +0000

    Baseline experiment

--- experiment_start.sh
+++ experiment_start.sh
@@ -3,20 +3,19 @@ kubectl scale --replicas=$2 statefulset/emqx -n dtwins
 
 sleep 80
 
-kubectl scale --replicas=1 deployments/cluster-manager -n dtwins
+kubectl scale --replicas=0 deployments/cluster-manager -n dtwins
 
 sleep 60
 
-kubectl scale --replicas=$1 statefulset/twin-ncv2x-stateful -n dtwins
+kubectl scale --replicas=$1 statefulset/twin-well-stateful -n dtwins
 kubectl scale --replicas=$1 statefulset/twin-dab-stateful -n dtwins
 kubectl scale --replicas=$1 statefulset/twin-das-stateful -n dtwins
 kubectl scale --replicas=$1 statefulset/twin-mc-stateful -n dtwins
-kubectl scale --replicas=$1 statefulset/twin-v2x-stateful -n dtwins
 
 sleep 60
 
-kubectl scale --replicas=$1 statefulset/client-ncv2x-stateful -n dtwins
+kubectl scale --replicas=$1 statefulset/client-well-stateful -n dtwins
 kubectl scale --replicas=$1 statefulset/client-dab-stateful -n dtwins
 kubectl scale --replicas=$1 statefulset/client-das-stateful -n dtwins
 kubectl scale --replicas=$1 statefulset/client-mc-stateful -n dtwins
-kubectl scale --replicas=$1 statefulset/client-v2x-stateful -n dtwins
+
--- logs.sh
+++ logs.sh
@@ -1,20 +1,19 @@
 
 mkdir logs_$1
 cd logs_$1
-mkdir logs_$1/mc 
-mkdir logs_$1/v2x
-mkdir logs_$1/dab
-mkdir logs_$1/das
-mkdir logs_$1/ncv2x
+mkdir raw
+cd raw
+mkdir mc 
+mkdir dab
+mkdir das
+mkdir well
 
 
-for type in mc v2x dab das ncv2x
+for type in mc dab das well
 do
 
-    for num in 0 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15
+    for num in 0 1 2 3 4 
     do 
     kubectl logs -n dtwins twin-${type}-stateful-${num} > ${type}/twin-${type}-stateful-${num}.log
     done
-    python3 ../main.py
-    
 done
