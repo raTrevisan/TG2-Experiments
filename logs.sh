1dd656e20b3861babab23f70870c22a672e9af09
Rafael Trevisan <rafa.trevi@hotmail.com>
2024-11-18 13:36:37 -0300

    experiments updates

--- logs.sh
+++ logs.sh
@@ -4,11 +4,12 @@ mkdir $1
 cd $1
 mkdir raw
 cd raw
+mkdir tcp
+mkdir quic
 for type in tcp quic
-mkdir ${type}
 do
     for num in 0 1 2 3 4 5 6 7 8 9
     do 
-    kubectl logs -n dtwins mqtt-${type}-${num} > ${type}/mqtt-${type}-${num}.log
+    kubectl logs -n dtwins mqtt-${type}-sub-${num} > ${type}/mqtt-${type}-${num}.log
     done
 done
