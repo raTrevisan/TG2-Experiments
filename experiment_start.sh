db34cffac2d3f103785ce89377765d93af8b73f1
Rafael <rafael.trevisan@inf.ufrgs.br>
2024-11-25 03:44:09 +0000

    quic double broker test

--- experiment_start.sh
+++ experiment_start.sh
@@ -8,13 +8,15 @@ sleep 80
 #sleep 60
 
 echo "Scaling Subscribers..."
-kubectl scale --replicas=$1 statefulset/mqtt-quic-type-1-sub -n dtwins
-kubectl scale --replicas=$1 statefulset/mqtt-quic-type-2-sub -n dtwins
-kubectl scale --replicas=$1 statefulset/mqtt-quic-type-3-sub -n dtwins
+kubectl scale --replicas=$1 statefulset/mqtt-quic-sub -n dtwins
+#kubectl scale --replicas=$1 statefulset/mqtt-quic-type-1-sub -n dtwins
+#kubectl scale --replicas=$1 statefulset/mqtt-quic-type-2-sub -n dtwins
+#kubectl scale --replicas=$1 statefulset/mqtt-quic-type-3-sub -n dtwins
 sleep 60
 
 echo "Scaling Clients..."
-kubectl scale --replicas=$3 statefulset/mqtt-quic-type-1-pub -n dtwins
-kubectl scale --replicas=$3 statefulset/mqtt-quic-type-2-pub -n dtwins
-kubectl scale --replicas=$3 statefulset/mqtt-quic-type-3-pub -n dtwins
+kubectl scale --replicas=$1 statefulset/mqtt-quic-pub -n dtwins
+#kubectl scale --replicas=$3 statefulset/mqtt-quic-type-1-pub -n dtwins
+#kubectl scale --replicas=$3 statefulset/mqtt-quic-type-2-pub -n dtwins
+#kubectl scale --replicas=$3 statefulset/mqtt-quic-type-3-pub -n dtwins
 echo "Done"
--- logs_test.sh
+++ logs_test.sh
@@ -5,5 +5,5 @@ cd logs_$1
 
 for num in 0 1 2 3 4 5 6 7 8 9 10
 do 
-kubectl logs -n dtwins mqtt-quic-sub-${num} > raw/twin-${type}-stateful-${num}.log
+kubectl logs -n dtwins mqtt-quic-sub-${num} > ./twin-${type}-stateful-${num}.log
 done
